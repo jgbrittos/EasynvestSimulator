ccfd04e489d974ca951b4c70ddb5ed4fb5c3ddda
João Gabriel <joaogabrielsilva@wizsolucoes.com.br>
2018-12-14 17:10:17 -0200

    Initial Commit

---
+++ EasynvestSimulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasynvestSimulator
+//
+//  Created by João Gabriel on 14/12/18.
+//  Copyright © 2018 João Gabriel de Britto e Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
